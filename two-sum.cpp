b53c6afa5d11ef0fb552125e64da7a12dd7b324c
Kavya-202401090 <202401090@daiict.ac.in>
2025-04-04 23:39:21 +0530

    Update two-sum.cpp

--- two-sum.cpp
+++ two-sum.cpp
@@ -1,31 +1,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        
-        vector<pair<int, int>> v;
-        vector<int> v2;
-        pair<int, int> p;
-        for(int i=0; i<nums.size(); i++){
-            p.first = nums[i];
-            p.second = i;
-            v.push_back(p);
-        }
-        sort(v.begin(), v.end());
-        auto it = v.begin(), it2 = v.end();
-        it2 --;
-        while(it < it2){
-            int sum = it->first + it2->first;
-            if(sum == target){
-                v2.push_back(it->second);
-                v2.push_back(it2->second);
-                return v2;
+        unordered_map<int, int> mp;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            int se = target - nums[i];
+            if (mp.count(se)) {
+                return {mp[se], i};
             }
-            else if(sum < target)
-                it ++;
-            else 
-                it2 --;
+            mp[nums[i]] = i;
         }
-            
-        return v2;
+        return {}; 
     }
 };
